f468b9cf73e1e5e1732b4cc7b33c2f13ed10f00f
Hilary Richardson <hilary.richardson@gmail.com>
2021-03-21 14:38:20 -0700

    adding validation for new animal

--- server.js
+++ server.js
@@ -28,6 +28,23 @@ function createNewAnimal(body, animalsArray) {
     return animal;
 }
 
+// Validate new animals
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
 // Filter by Trait Query function
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -95,10 +112,15 @@ app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
 
-    // add animal to json file and animals array in this function
-    const animal = createNewAnimal(req.body, animals);
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
 
-    res.json(animal);
+        res.json(animal);
+    }
 
 });
 
